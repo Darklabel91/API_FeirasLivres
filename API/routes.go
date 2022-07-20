137caa9d364810d9fc9719c2593063d5c222e005
Daniel Fillol <55287657+Darklabel91@users.noreply.github.com>
2022-07-19 22:15:46 -0300

    Create contentType function that apply json in response header

--- API/routes.go
+++ API/routes.go
@@ -8,10 +8,18 @@ import (
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(contentType)
 	r.HandleFunc("/api/fairs/{searchType}/{searchParam}", GetFair).Methods("Get")
 	r.HandleFunc("/api/fairs/id/{searchParam}", DeleteFair).Methods("Delete")
 	r.HandleFunc("/api/fairs/id/{searchParam}", UpdateFair).Methods("Put")
 	r.HandleFunc("/api/fairs", CreateFair).Methods("Post")
 	r.HandleFunc("/api/fairs", GetFairs).Methods("Get")
 	log.Fatal(http.ListenAndServe("localhost:8000", r))
+}
+
+func contentType(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
