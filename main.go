c28a73bc5adfe0548929cfbf549a44e44616cf1e
Daniel Fillol <55287657+Darklabel91@users.noreply.github.com>
2022-07-19 22:18:18 -0300

    Create writeLog that generates logs.txt

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"github.com/Darklabel91/API_FeirasLivres/API"
 	"github.com/Darklabel91/API_FeirasLivres/database"
+	"github.com/Darklabel91/API_FeirasLivres/writeLog"
+	"log"
 )
 
 const path = "database/migration/DEINFO_AB_FEIRASLIVRES_2014.csv"
 
 func main() {
+	//Set Log
+	writeLog.WriteLog()
+	log.Println("Start API")
+
 	//Load Database
 	database.Connect()
 
---
+++ writeLog/writeLog.go
@@ -0,0 +1,15 @@
+package writeLog
+
+import (
+	"log"
+	"os"
+)
+
+func WriteLog() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.SetOutput(file)
+}
