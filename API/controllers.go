f3635d687e729e72d164b40afefa1d685a07032d
Daniel Fillol <55287657+Darklabel91@users.noreply.github.com>
2022-07-20 18:55:31 -0300

    JSON response on exceptions

--- API/controllers.go
+++ API/controllers.go
@@ -16,23 +16,23 @@ func CreateFair(w http.ResponseWriter, r *http.Request) {
 	var newFair models.Fair
 	err := json.NewDecoder(r.Body).Decode(&newFair)
 	if err != nil {
-		log.Println("ERROR: could not decode JSON file in: http://localhost:8000/api/fairs on POST request")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		msgErr := "ERROR: could not decode JSON file in: http://localhost:8000/api/fairs on POST request"
+		jsonErrMsg(w, msgErr)
 	}
 
 	err = models.ValidateFair(&newFair)
 	if err != nil {
-		log.Println("ERROR: JSON file error, all fields must have a value")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		msgErr := "ERROR: JSON file error, all fields must have a value"
+		jsonErrMsg(w, msgErr)
 	}
 
-	database.DB.Create(&newFair)
-	err = json.NewEncoder(w).Encode(newFair)
+	createFair := models.Fair{Longitude: newFair.Longitude, Latitude: newFair.Latitude, SetCen: newFair.SetCen, AreaP: newFair.AreaP, CodDist: newFair.CodDist, District: newFair.District, CodSubPref: newFair.CodSubPref, SubPref: newFair.SubPref, RegionFive: newFair.RegionFive, RegionEight: newFair.RegionEight, NameFair: newFair.NameFair, Record: newFair.Record, Street: newFair.Street, Number: newFair.Number, Neighbourhood: newFair.Neighbourhood, Reference: newFair.Reference}
+	database.DB.Create(&createFair)
+
+	err = json.NewEncoder(w).Encode(createFair)
 	if err != nil {
-		log.Println("ERROR: could not encode JSON file in: http://localhost:8000/api/fairs on POST request")
-		w.WriteHeader(http.StatusInternalServerError)
+		msgErr := "ERROR: could not encode JSON file in: http://localhost:8000/api/fairs on POST request"
+		jsonErrMsg(w, msgErr)
 	}
 
 	log.Println("INFO: post request http://localhost:8000/api/fairs new fair created")
@@ -49,13 +49,13 @@ func DeleteFair(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewEncoder(w).Encode(fair)
 	if err != nil {
-		log.Println("ERROR: could not encode JSON file in: http://localhost:8000/api/fairs on DELETE request")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		msgErr := "ERROR: could not encode JSON file in: http://localhost:8000/api/fairs on DELETE request"
+		jsonErrMsg(w, msgErr)
 	}
 
-	log.Println("INFO: fair with id:" + id + " DELETED")
-	w.WriteHeader(http.StatusAccepted)
+	msg := "INFO: fair with id:" + id + " DELETED"
+	log.Println(msg)
+	_ = json.NewEncoder(w).Encode(msg)
 }
 
 //UpdateFair update models.Fair by given id
@@ -68,33 +68,30 @@ func UpdateFair(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&fair)
 	if err != nil {
-		log.Println("ERROR: could not decode JSON file in: http://localhost:8000/api/fairs on PUT request")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		msgErr := "ERROR: could not decode JSON file in: http://localhost:8000/api/fairs on PUT request"
+		jsonErrMsg(w, msgErr)
 	}
 
 	err = models.ValidateFair(&fair)
 	if err != nil {
-		log.Println("ERROR: JSON file error, all fields must have a value")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		msgErr := "ERROR: JSON file error, all fields must have a value"
+		jsonErrMsg(w, msgErr)
 	}
 
 	//Probably a way to do it simpler
 	sameID, err := strconv.Atoi(id)
 	if err != nil {
-		log.Println("ERROR: could not convert string to int in: http://localhost:8000/api/fairs on PUT request")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		msgErr := "ERROR: could not convert string to int in: http://localhost:8000/api/fairs on PUT request"
+		jsonErrMsg(w, msgErr)
 	}
-	updateFair := models.Fair{Id: sameID, Longitude: fair.Longitude, Latitude: fair.Latitude, SetCen: fair.SetCen, AreaP: fair.AreaP, CodDist: fair.CodDist, District: fair.District, CodSubPref: fair.CodSubPref, SubPref: fair.SubPref, RegionFive: fair.RegionFive, RegionEight: fair.RegionEight, NameFair: fair.NameFair, Record: fair.Record, Street: fair.Street, Number: fair.Number, Neighbourhood: fair.Neighbourhood, Reference: fair.Reference}
 
+	updateFair := models.Fair{Id: sameID, Longitude: fair.Longitude, Latitude: fair.Latitude, SetCen: fair.SetCen, AreaP: fair.AreaP, CodDist: fair.CodDist, District: fair.District, CodSubPref: fair.CodSubPref, SubPref: fair.SubPref, RegionFive: fair.RegionFive, RegionEight: fair.RegionEight, NameFair: fair.NameFair, Record: fair.Record, Street: fair.Street, Number: fair.Number, Neighbourhood: fair.Neighbourhood, Reference: fair.Reference}
 	database.DB.Save(&updateFair)
+
 	err = json.NewEncoder(w).Encode(updateFair)
 	if err != nil {
-		log.Println("ERROR: could not encode JSON file in: http://localhost:8000/api/fairs on PUT request")
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		msgErr := "ERROR: could not encode JSON file in: http://localhost:8000/api/fairs on PUT request"
+		jsonErrMsg(w, msgErr)
 	}
 
 	log.Println("INFO: put request http://localhost:8000/api/fairs fair updated with ID:" + id)
@@ -109,18 +106,33 @@ func GetFair(w http.ResponseWriter, r *http.Request) {
 
 	switch searchType {
 	case "district":
-		_ = SearchFair(w, searchParam, searchType, "district = ?")
+		err := SearchFair(w, searchParam, searchType, "district = ?")
+		if err != nil {
+			return
+		}
 	case "region":
-		_ = SearchFair(w, searchParam, searchType, "region_five = ?")
+		err := SearchFair(w, searchParam, searchType, "region_five = ?")
+		if err != nil {
+			return
+		}
 	case "name":
-		_ = SearchFair(w, searchParam, searchType, "name_fair = ?")
+		err := SearchFair(w, searchParam, searchType, "name_fair = ?")
+		if err != nil {
+			return
+		}
 	case "neighbourhood":
-		_ = SearchFair(w, searchParam, searchType, "neighbourhood = ?")
+		err := SearchFair(w, searchParam, searchType, "neighbourhood = ?")
+		if err != nil {
+			return
+		}
 	case "id":
-		_ = SearchFair(w, searchParam, searchType, "id = ?")
+		err := SearchFair(w, searchParam, searchType, "id = ?")
+		if err != nil {
+			return
+		}
 	default:
-		log.Println("WARNING: searchType: " + searchType + " unexpected. searchType must be [district,region,name or neighbourhood]	http://localhost:8000/api/fairs/" + searchType + "/" + searchParam)
-		w.WriteHeader(http.StatusBadRequest)
+		msgErr := "WARNING: searchType: " + searchType + " unexpected. searchType must be [district,region,name or neighbourhood]"
+		jsonErrMsg(w, msgErr)
 	}
 }
 
@@ -131,15 +143,15 @@ func SearchFair(w http.ResponseWriter, searchParam, searchType, query string) er
 
 	database.DB.Select("*").Where(query, searchParam).Table("fairs").Find(&foundFairs).Count(&totalFairs)
 	if totalFairs < 1 {
-		log.Println("WARNING: http://localhost:8000/api/fairs/" + searchType + "/" + searchParam + " have return 0")
-		w.WriteHeader(http.StatusNoContent)
+		msgErr := "WARNING: http://localhost:8000/api/fairs/" + searchType + "/" + searchParam + " have return 0"
+		jsonErrMsg(w, msgErr)
 		return errors.New(searchParam + " not found")
 	}
 
 	err := json.NewEncoder(w).Encode(foundFairs)
 	if err != nil {
-		log.Println("ERROR: could not encode JSON file in: http://localhost:8000/api/fairs/" + searchType + "/" + searchParam)
-		w.WriteHeader(http.StatusInternalServerError)
+		msgErr := "ERROR: could not encode JSON file in: http://localhost:8000/api/fairs/" + searchType + "/" + searchParam
+		jsonErrMsg(w, msgErr)
 		return err
 	}
 
@@ -154,17 +166,20 @@ func GetFairs(w http.ResponseWriter, r *http.Request) {
 	database.DB.Find(&fairs).Count(&totalFairs)
 
 	if totalFairs < 1 {
-		log.Println("WARNING: get request to http://localhost:8000/api/fairs	have return 0")
-		w.WriteHeader(http.StatusNoContent)
-		return
+		msgErr := "WARNING: get request to http://localhost:8000/api/fairs	have return 0"
+		jsonErrMsg(w, msgErr)
 	} else {
 		err := json.NewEncoder(w).Encode(fairs)
 		if err != nil {
-			log.Println("ERROR: could not encode JSON file in: http://localhost:8000/api/fairs")
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+			msgErr := "ERROR: could not encode JSON file in: http://localhost:8000/api/fairs"
+			jsonErrMsg(w, msgErr)
 		}
 
 		log.Println("INFO: get request to http://localhost:8000/api/fairs" + "	fairs found:" + strconv.Itoa(int(totalFairs)))
 	}
+}
+
+func jsonErrMsg(w http.ResponseWriter, msgErr string) {
+	log.Println(msgErr)
+	_ = json.NewEncoder(w).Encode(msgErr)
 }
